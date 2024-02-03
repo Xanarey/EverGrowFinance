068e9fde9dc23db13b70f50e0533daa73460840b
Xanarey <doctor.layout@mail.ru>
2024-02-03 19:15:10 +0300

    Initial commit

---
+++ src/main/java/com/swift/evergrowfinance/EverGrowFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.swift.evergrowfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverGrowFinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverGrowFinanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swift/evergrowfinance/EverGrowFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swift.evergrowfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverGrowFinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
