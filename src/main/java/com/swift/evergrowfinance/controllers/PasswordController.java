c3d35e3239a95976f5893f7254da87f1c1f1443b
xanarey <doctor.layout@mail.ru>
2025-02-15 15:13:45 +0300

    passwordEntity

--- src/main/java/com/swift/evergrowfinance/controllers/PasswordController.java
+++ src/main/java/com/swift/evergrowfinance/controllers/PasswordController.java
@@ -2,7 +2,6 @@ package com.swift.evergrowfinance.controllers;
 
 import com.swift.evergrowfinance.dto.PasswordForgotDTO;
 import com.swift.evergrowfinance.dto.PasswordResetDTO;
-import com.swift.evergrowfinance.service.EmailService;
 import com.swift.evergrowfinance.service.PasswordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,32 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class PasswordController {
 
     private final PasswordService passwordService;
-    private final EmailService emailService;
 
     @Autowired
-    public PasswordController(PasswordService passwordService, EmailService emailService) {
+    public PasswordController(PasswordService passwordService) {
         this.passwordService = passwordService;
-        this.emailService = emailService;
     }
 
     @PostMapping("/forgot")
     public ResponseEntity<?> processForgotPassword(@RequestBody PasswordForgotDTO passwordForgotDTO) {
-        String token = passwordService.createPasswordResetToken(passwordForgotDTO.getEmail());
-        if (token != null) {
-            emailService.sendPasswordResetEmail(passwordForgotDTO.getEmail(), token);
-            return ResponseEntity.ok("Password reset email sent");
-        } else {
-            return ResponseEntity.badRequest().body("Email not found");
-        }
+        return passwordService.getStringResponseEntity(passwordForgotDTO);
     }
 
     @PostMapping("/reset")
     public ResponseEntity<?> processResetPassword(@RequestBody PasswordResetDTO passwordResetDTO) {
-        boolean result = passwordService.resetPassword(passwordResetDTO.getToken(), passwordResetDTO.getPassword());
-        if (result) {
-            return ResponseEntity.ok("Password reset successful");
-        } else {
-            return ResponseEntity.badRequest().body("Invalid or expired token");
-        }
+        return passwordService.getStringResetPasswordEntity(passwordResetDTO);
     }
+
+
 }
--- src/main/java/com/swift/evergrowfinance/service/PasswordService.java
+++ src/main/java/com/swift/evergrowfinance/service/PasswordService.java
@@ -1,10 +1,13 @@
 package com.swift.evergrowfinance.service;
 
+import com.swift.evergrowfinance.dto.PasswordForgotDTO;
+import com.swift.evergrowfinance.dto.PasswordResetDTO;
 import com.swift.evergrowfinance.model.entities.PasswordResetToken;
 import com.swift.evergrowfinance.model.entities.User;
 import com.swift.evergrowfinance.repository.PasswordResetTokenRepository;
 import com.swift.evergrowfinance.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,12 +21,33 @@ public class PasswordService {
     private final UserRepository userRepository;
     private final PasswordResetTokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
 
     @Autowired
-    public PasswordService(UserRepository userRepository, PasswordResetTokenRepository tokenRepository, PasswordEncoder passwordEncoder) {
+    public PasswordService(UserRepository userRepository, PasswordResetTokenRepository tokenRepository, PasswordEncoder passwordEncoder, EmailService emailService) {
         this.userRepository = userRepository;
         this.tokenRepository = tokenRepository;
         this.passwordEncoder = passwordEncoder;
+        this.emailService = emailService;
+    }
+
+    public ResponseEntity<String> getStringResponseEntity(PasswordForgotDTO passwordForgotDTO) {
+        String token = createPasswordResetToken(passwordForgotDTO.getEmail());
+        if (token != null) {
+            emailService.sendPasswordResetEmail(passwordForgotDTO.getEmail(), token);
+            return ResponseEntity.ok("Password reset email sent");
+        } else {
+            return ResponseEntity.badRequest().body("Email not found");
+        }
+    }
+
+    public ResponseEntity<String> getStringResetPasswordEntity(PasswordResetDTO passwordResetDTO) {
+        boolean result = resetPassword(passwordResetDTO.getToken(), passwordResetDTO.getPassword());
+        if (result) {
+            return ResponseEntity.ok("Password reset successful");
+        } else {
+            return ResponseEntity.badRequest().body("Invalid or expired token");
+        }
     }
 
     public String createPasswordResetToken(String email) {
